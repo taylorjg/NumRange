103779ab1039a224fb702044313a55ac2ca29ae9
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-08-11 21:11:24 +0100

    Initial commit

---
+++ Code/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Code
+{
+    public static class StringExtension
+    {
+        public static IEnumerable<int> ToNumList(this string s)
+        {
+            var result = new List<int>();
+            if (!string.IsNullOrEmpty(s)) result.AddRange(s.Split(',').SelectMany(ParseRange));
+            return result;
+        }
+
+        private static IEnumerable<int> ParseRange(string r)
+        {
+            var ps = r.Split('-');
+            var a = int.Parse(ps[0]);
+            var b = ps.Length == 2 ? int.Parse(ps[1]) : a;
+            return Enumerable.Range(a, b - a + 1);
+        }
+    }
+}
---
+++ Tests/StringExtensionTests.cs
@@ -0,0 +1,51 @@
+﻿using Code;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class StringExtensionTests
+    {
+        [Test]
+        public void NullStringReturnsEmptyList()
+        {
+            var actual = (null as string).ToNumList();
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void EmptyStringReturnsEmptyList()
+        {
+            var actual = "".ToNumList();
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void StringContainingOneNumReturnsAListContainingOneNum()
+        {
+            var actual = "12".ToNumList();
+            Assert.That(actual, Is.EqualTo(new[] {12}));
+        }
+
+        [Test]
+        public void StringContainingTwoNumsReturnsAListContainingTwoNums()
+        {
+            var actual = "12,13".ToNumList();
+            Assert.That(actual, Is.EqualTo(new[] {12, 13}));
+        }
+
+        [Test]
+        public void StringContainingOneRangeReturnsAListContainingOneRange()
+        {
+            var actual = "2-5".ToNumList();
+            Assert.That(actual, Is.EqualTo(new[] {2, 3, 4, 5}));
+        }
+
+        [Test]
+        public void ComplexTest()
+        {
+            var actual = "1-12,14,16".ToNumList();
+            Assert.That(actual, Is.EqualTo(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16}));
+        }
+    }
+}
