99b2443dc4cff947d60a97a6ea2583b47efdcb6c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-08-11 21:34:18 +0100

    Slightly shorter version

--- Code/StringExtension.cs
+++ Code/StringExtension.cs
@@ -7,15 +7,13 @@ namespace Code
     {
         public static IEnumerable<int> ToNumList(this string s)
         {
-            return !string.IsNullOrEmpty(s) ? s.Split(',').SelectMany(ParseRange) : Enumerable.Empty<int>();
-        }
-
-        private static IEnumerable<int> ParseRange(string r)
-        {
-            var ns = r.Split('-').Select(int.Parse).ToArray();
-            var a = ns[0];
-            var b = ns.Length > 1 ? ns[1] : a;
-            return Enumerable.Range(a, b - a + 1);
+            return !string.IsNullOrEmpty(s) ? s.Split(',').SelectMany(r =>
+                {
+                    var ns = r.Split('-').Select(int.Parse).ToArray();
+                    var a = ns[0];
+                    var b = ns.Length > 1 ? ns[1] : a;
+                    return Enumerable.Range(a, b - a + 1);
+                }) : Enumerable.Empty<int>();
         }
     }
 }
